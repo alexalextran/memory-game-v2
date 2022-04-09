6994094ffc859d75634af4d73466d283f5c5c9e7
alexalextran <alextran8385@gmail.com>
2022-04-09 15:41:37 +1000

    game wrapper, header and home button

--- src/Components/Game.jsx
+++ src/Components/Game.jsx
@@ -3,7 +3,22 @@ import React from 'react';
 const Game = () => {
     return (
         <div>
-            game :)
+            <div className='game__wrapper'>
+            <div className='game__wrapper--header'>
+                 <div>
+                    <button>Back Home</button>
+                 </div>
+                 <div className='header__info'>
+                   <ul>
+                       <li>Level <span>0</span></li>
+                       <li>Boxes <span>3</span></li>
+                       <li>Lives <span>0</span></li>
+                       <li>Time <span>3</span></li>
+                   </ul>
+                 </div>
+            </div>
+               
+            </div>
         </div>
     );
 }
