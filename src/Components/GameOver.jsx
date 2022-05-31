62b50438bbeb616e2a4eb5d0cc25a9bc2ec25740
Alex Tran <alextran8385@gmail.com>
2022-05-31 14:36:57 +1000

    users are now able to add a highscore if first time playing

--- src/Components/GameOver.jsx
+++ src/Components/GameOver.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useAuth } from '../Contexts/AuthContext';
-import { collection, doc, getDoc, getDocs, getFirestore, setDoc } from "firebase/firestore";
+import { collection, doc, getDoc, getDocs, getFirestore, setDoc } from 'firebase/firestore/lite';
 
 const GameOver = (props) => {
    const { currentUser } = useAuth()
    const db = getFirestore();
    const docRef = doc(db, `users`, `${currentUser.uid}`);
-   var newScore = false;
+   const [loading, setLoading] = useState(true)
+   const [newScore, setnewScore] = useState(false)
    
  //get firestore document
 
@@ -15,20 +16,49 @@ const GameOver = (props) => {
         if (snapshot.exists()) {
        console.log(snapshot.data() )
         } else{
-            newScore = true;
+            setnewScore(true);
             console.log("does not exist")
         }
+        setLoading(false)
     })
     
+    function addHighScore(){
+        setDoc(doc(db, "users", `${currentUser.uid}`),{
+            Level: props.highscore.level,
+            Time: props.highscore.time
+        })
+        console.log("highscore added")
+    }
 
 
     return (
+        <>
+        {
+            loading ?
+            <div>
+                loading
+            </div>
+            :
         <div className='gameOver'>
             <h1>Game Over</h1>
             <p>Nice Job, you got up to level {props.highscore.level} with a time of {props.highscore.time} seconds</p>
+            {
+                newScore ?
+                <>
+                    <p>Woah you got a new highscore! would you like to update your highscore?</p>
+                    <button className='button' onClick={() => addHighScore()}>Update</button>    </>
+                 :
+                 <>
+                <p>No new highscore</p>
+                </>
+                   
 
-            
+            }
+
         </div>
+        }
+        </>
+
     );
 }
 
