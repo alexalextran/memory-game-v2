6dd4e0bc875ac98d40f03ae56e0ad9cd2cb711cf
Alex Tran <alextran8385@gmail.com>
2022-06-07 22:34:23 +1000

    final changes (added more tips)

--- src/Components/GameOver.jsx
+++ src/Components/GameOver.jsx
@@ -17,7 +17,9 @@ const GameOver = (props) => {
        "As each level goes on the time that each box is shown by increases by 100 miliseconds, so once you reach level 10 the time for each box is 2 seconds",
        "The max level you can reach for each gridnumber you choose is (gridnumber - 2) so if your grid is 9 squares the max level is 7 (this is because you can't lose once you reach level 7",
        "The gridsystem allows for high risk high reward since while it is easier to play using a smaller gridnumber, you are limited in the highscore you can reach",
-       "Level has priority over time on the leaderboard, so feel free to take your time!"
+       "Level has priority over time on the leaderboard, so feel free to take your time!",
+       "Once you near the end of your max grid level, it may be easier to focus on the squares that are not chosen",
+       "It may help to designate an order on which square to click"
     ]
   
  //get firestore document
